33efb450718d780a251b096552080cf965a977df
Felipe Rolon <afra.9622@gmail.com>
2023-03-27 00:30:42 -0400

    Nice looking

--- components/CrepesList.js
+++ components/CrepesList.js
@@ -1,44 +1,90 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
 import crepesData from './CrepesData';
 
-const CrepesMenu = () => {
+const CrepesList = () => {
   return (
-    <View style={styles.container}>
-      {crepesData.map((crepe, index) => (
-        <View style={styles.menuItem} key={index}>
-          <Text style={styles.crepeName}>{crepe.crepeName}</Text>
-          {Object.keys(crepe.crepeIngredients).map((ingredient, index) => (
-            <Text key={index} style={styles.ingredient}>
-              - {ingredient}: {crepe.crepeIngredients[ingredient]}
-            </Text>
+    <ImageBackground
+      source={require('../assets/background.jpg')}
+      style={styles.backgroundImage}
+    >
+      <LinearGradient
+        colors={['rgba(255, 255, 255, 0.3)', 'rgba(255, 255, 255, 0.9)']}
+        style={styles.linearGradient}
+      >
+        <View style={styles.container}>
+          {crepesData.map((crepe, index) => (
+            <View style={styles.menuItem} key={index}>
+              <Text style={styles.title}>{crepe.crepeName}</Text>
+              <View style={styles.ingredientsContainer}>
+                {Object.entries(crepe.crepeIngredients).map(([ingredient, description], index) => (
+                  <View style={styles.ingredientItem} key={index}>
+                    <Text style={styles.ingredientName}>{ingredient}</Text>
+                    <Text style={styles.ingredientDescription}>{description}</Text>
+                  </View>
+                ))}
+              </View>
+            </View>
           ))}
         </View>
-      ))}
-    </View>
+      </LinearGradient>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  linearGradient: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingTop: 40,
+    paddingBottom: 20,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexDirection: 'column',
     justifyContent: 'center',
-    padding: 20,
   },
   menuItem: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 20,
     marginBottom: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+    elevation: 7,
   },
-  crepeName: {
-    fontSize: 22,
+  title: {
     fontWeight: 'bold',
+    fontSize: 20,
     marginBottom: 10,
+    textAlign: 'center',
   },
-  ingredient: {
-    fontSize: 18,
+  ingredientsContainer: {
+    flexDirection: 'column',
+  },
+  ingredientItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     marginBottom: 5,
+  },
+  ingredientName: {
+    fontWeight: 'bold',
+    flex: 1,
+  },
+  ingredientDescription: {
+    flex: 2,
   },
 });
 
-export default CrepesMenu;
+export default CrepesList;
